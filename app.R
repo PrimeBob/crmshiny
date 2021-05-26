9a630a6883c4d710f4569229086d1019017bb8c9
PrimeBob <73451346+PrimeBob@users.noreply.github.com>
2021-05-26 13:11:39 +1000

    Update app.R
    can now take more complicated queries (multiple inputs, output alike to input)

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+5/26
+
+
 library(shiny)
 library(sqldf)
 
@@ -26,8 +29,33 @@ server <- function(input, output) {
       paste0("output.csv")
     },
     content = function(file) {
-     
+
       
+      #putting the title inputs into a vector
+      titlesparses <- character(length(input$titles))
+      
+      #loop for concatentating the complementary sql code to the inputs
+      for (i in 2:length(input$titles))
+      {
+        if (i < length(input$titles))
+        {
+          titlesparses[i] <- paste0("title like '%", input$titles[i], "%' OR ")
+        }
+        else if (i == length(input$titles))
+        {
+          titlesparses[i] <- paste0("title like '%", input$titles[i], "%'")
+        }
+      }
+      #combing all elements int he list to one string
+      titlescombined = paste0(titlesparses, collapse  = "")
+      
+      
+      
+      
+
+           
+      setwd("~/Desktop/rserver")
+
       #local database
       q1 <- read.csv("crm2019-2020legit.csv")
       
@@ -37,7 +65,8 @@ server <- function(input, output) {
           paste0(
             "select distinct first, last, title, company, email from q1 where title like '%",
             input$titles,
-            "%'"
+            "%' OR ",
+            titlescombined
           )
           
           
