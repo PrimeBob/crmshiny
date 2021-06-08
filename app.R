914c965c4b1717461f3d2cd37453af79fbd00388
jason <73451346+PrimeBob@users.noreply.github.com>
2021-06-08 11:53:02 +1000

    Update app.R
    -now geared for a local server
    -can now filter by domains via csv upload

--- app.R
+++ app.R
@@ -1,6 +1,3 @@
-5/26
-
-
 library(shiny)
 library(sqldf)
 
@@ -9,33 +6,53 @@ ui <- fluidPage(
   checkboxGroupInput(
     "titles",
     "Tick the titles you want",
-    c("it", "marketing", "finance", "data", "human resources")
-  ),
-  checkboxGroupInput("region", "Tick the regions you want", c("ANZ", "EMEA", "SEA")),
-  checkboxGroupInput(
-    "seniority",
-    "Tick the seniority you want",
-    c("Chief", "Director", "VP", "Senior", "Head")
-  ),
+    c("IT ", "Data", "Information", "Tech", "Arch", "Human Resources", "Marketing", "Finance")),
+  checkboxGroupInput(  
+    "regions", 
+    "Tick the regions you want",
+    c("ANZ", "EMEA", "SEA", "DACH", "NAMER")),
+  
+  fileInput('target_upload', 'Upload domain here',
+            accept = c(
+              'text/csv',
+              'text/comma-separated-values',
+              '.csv'
+            )),
+  DT::dataTableOutput("sample_table"),
+  
+
+  
   downloadButton("downloadData", "Download"),
+  
+  
 )
 
 
 
 
 server <- function(input, output) {
-  output$downloadData <- downloadHandler(
+
+    output$downloadData <- downloadHandler(
     filename = function() {
       paste0("output.csv")
     },
     content = function(file) {
-
+      
+      
+      
+      #local database
+      setwd("~/Desktop/rserver")
+      
+      
+      q0 <- read.csv("crm2019-2020legit.csv")
+      
+      #titles
       
       #putting the title inputs into a vector
       titlesparses <- character(length(input$titles))
       
       #loop for concatentating the complementary sql code to the inputs
-      for (i in 2:length(input$titles))
+      for (i in 1:length(input$titles))
       {
         if (i < length(input$titles))
         {
@@ -52,26 +69,90 @@ server <- function(input, output) {
       
       
       
-
-           
-      setwd("~/Desktop/rserver")
-
-      #local database
-      q1 <- read.csv("crm2019-2020legit.csv")
+      q1 <-
+        sqldf(
+          paste0(
+            "select distinct first, last, title, company, region, email from q0 where ",
+            titlescombined
+          )
+        )
       
-      #query on local database, pasting parts of the query as string
+      
+      #region
+      
+      regionsparses <- character(length(input$regions))
+      
+      #loop for concatentating the complementary sql code to the inputs
+      for (i in 1:length(input$regions))
+      {
+        if (i < length(input$regions))
+        {
+          regionsparses[i] <- paste0("region like '%", input$regions[i], "%' OR ")
+        }
+        else if (i == length(input$regions))
+        {
+          regionsparses[i] <- paste0("region like '%", input$regions[i], "%'")
+        }
+      }
+      #combing all elements int he list to one string
+      regionscombined = paste0(regionsparses, collapse  = "")
+      
+      
+      
+      
       q2 <-
         sqldf(
           paste0(
-            "select distinct first, last, title, company, email from q1 where title like '%",
-            input$titles,
-            "%' OR ",
-            titlescombined
+            "select distinct first, last, title, company, region, email from q1 where ",
+            regionscombined
           )
-          
-          
         )
-      write.csv(q2, file)
+      
+      
+      
+      
+      #domains
+      domains <- reactive({
+        inFile <- input$target_upload
+        if (is.null(inFile))
+          return(NULL)
+        df <- read.csv(inFile$datapath, header = TRUE,sep = ",")
+        
+        
+        domainsparses <- character(length(df$domain))
+        
+        #loop for concatentating the complementary sql code to the inputs
+        for (i in 1:length(df$domain))
+        {
+          if (i < length(df$domain))
+          {
+            domainsparses[i] <- paste0("email like '%", "@", df$domain[i], "%' OR ")
+          }
+          else if (i == length(df$domain))
+          {
+            domainsparses[i] <- paste0("email like '%", "@", df$domain[i], "%'")
+          }
+        }
+        #combing all elements int he list to one string
+        domaincombined = paste0(domainsparses, collapse  = "")
+        
+        return(domaincombined)
+        
+      })
+
+      
+      q3 <-
+        sqldf(
+          paste0(
+            "select distinct first, last, title, company, region, email from q2 where ",
+            domains()
+          )
+        )
+            
+      
+      
+
+      write.csv(q3, file)
     }
   )
 }
@@ -81,3 +162,5 @@ server <- function(input, output) {
 
 
 shinyApp(ui, server)
+
+
